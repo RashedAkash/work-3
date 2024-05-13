ffd7963bb6ffbad58916db526d2cf7c767757710
hablu <131282586+hablu@users.noreply.github.com>
2024-05-13 09:02:25 +0600

    latest opportunity section create

--- src/components/latestOpportunity/LatestOpportunity.jsx
+++ src/components/latestOpportunity/LatestOpportunity.jsx
@@ -1,11 +1,28 @@
 import SectionTitle from "@/shared/SectionTitle";
+import { jobData } from "../../../public/data/data";
+import Container from "../container/Container";
 
 const LatestOpportunity = () => {
+ 
   return (
     <section>
-      <div className="text-center">
-        <SectionTitle title1="Latest" title2="opportunities" />
-      </div>
+      <Container>
+        <div className="text-center mb-[45px]">
+          <SectionTitle title1="Latest" title2="opportunities" />
+        </div>
+        <div className="grid lg:grid-cols-5 xl:grid-cols-5 gap-8">
+          {jobData?.map((job, index) => (
+            <div
+              className="custom-box-shadow bg-white rounded-[40px] w-[200px] h-[200px] relative"
+              key={index}
+            >
+              <p className=" font-semibold text-2xl primary-color absolute top-1/2 -translate-y-1/2 -translate-x-1/2  left-1/2 ">
+                {job?.title}
+              </p>
+            </div>
+          ))}
+        </div>
+      </Container>
     </section>
   );
 };
